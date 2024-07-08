29410bdc8547d95328729537bb19afd23a5bd634
Akshay Dhobale <akshaysd592@gamil.com>
2024-07-08 15:45:45 +0530

    next auth completed for sign in

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,87 @@
+import { NextAuthOptions } from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+
+import bcrypt from 'bcryptjs'
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+
+export const authOptions : NextAuthOptions = {
+   providers:[
+        CredentialsProvider({ // creadential are created so that there is a formation of page for this provided value such as github based login
+            id:"credentials",
+            name:"Credentails",
+            credentials:{ // for html form created automatically by next-auth
+                email : {label:"Email",type:"text"},
+                password:{password:"password",type:"password"}
+            },
+            async authorize(credentials:any):Promise<any>{
+                await dbConnect();
+                try { // to make authorize 
+                   const user  =  await UserModel.findOne({
+                        $or:[
+                            {
+                                email: credentials.identifier
+                            },
+                            {
+                                username:credentials.identifier
+                            }
+                        ]
+                    })
+
+                    if(!user) {
+                        throw new Error("No User found with this email ")
+                    }
+
+                    if(!user.isVerified){
+                        throw new Error("Please verify your account before login")
+                    }
+
+                  const isPasswordCorrect =   await bcrypt.compare(credentials.password,user.password)
+
+                  if(isPasswordCorrect){
+                    return user // according to documentation
+                  }else{
+                    throw new Error("Incorrect Password")
+                  }
+                    
+                } catch (error:any) {
+                    throw new Error(error)
+                }
+
+            }
+        }),
+    ],
+    callbacks:{ // here user will be proveded from providers/credentails
+        async jwt({ token, user }) { // token will have only id but we want more things to be stored in token so 
+                                    // costumising its structure inside types/next-auth.ts and also store same data in session also
+            if(user){ // changed structure like types/next-auth.d.ts for this storage of data 
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified
+                token.isAcceptingMessages= user.isAcceptingMessages
+                token.username  = user.username
+            }
+
+            return token 
+         },
+        async session({ session, token }) {
+
+            if(token){ // similar to above make interface changes
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+            }
+            return session
+        }
+        
+    },
+    pages:{
+        signIn:'/sign-in'
+    },
+    session:{
+        strategy:"jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+
+    
+}
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,32 @@
+// here changin next-auth original structure
+import 'next-auth'
+import { DefaultSession } from 'next-auth';
+
+ // here can't use interface because we are modifing next-auth framework structure
+declare module 'next-auth'{
+    interface User{
+        _id : string;
+        isVerified?: boolean;
+        isAcceptingMessages?:boolean;
+        username?:string
+    }
+    interface Session {
+       user:{
+        _id? :string;
+        isVerified?: boolean;
+        isAcceptingMessages?:boolean;
+        username?:string
+       } & DefaultSession['user'] 
+    }
+}
+
+// another way for interface declaration 
+
+declare module 'next-auth/jwt'{
+    interface JWT{
+        _id? : string;
+        isVerified?:boolean;
+        isAcceptingMessages?:boolean;
+        username?:string;
+    }
+}
